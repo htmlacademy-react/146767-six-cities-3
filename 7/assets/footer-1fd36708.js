a8fa96e468a00712c4da96d70649793dfc061f9c
keksobot <github+keksobot@htmlacademy.ru>
2025-04-21 11:26:40 +0000

    ✔️ Сборка #7

---
+++ 7/assets/footer-1fd36708.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b4ee692c.js";function e({children:o}){return r.jsx("footer",{className:"footer container",children:o})}export{e as F};
--- 7/assets/footer-7721c6cc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-4840c809.js";function e({children:o}){return r.jsx("footer",{className:"footer container",children:o})}export{e as F};
